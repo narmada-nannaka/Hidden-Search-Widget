cbd06e88790e5b46f291de5c7917bb4c94dfca23
narmada-nannaka <hello@narmadanannaka.com>
2020-12-11 05:28:10 +1000

    A small search bar widget that can be re-used across other projects.

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+const entry = document.querySelector('.entry');
+
+btn.addEventListener('click', () => {
+    container.classList.toggle('active');
+    entry.focus;
+});
+
